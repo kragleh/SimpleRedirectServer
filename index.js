518adfb3cfe8b2105afb7029031535adbdde117f
kragleh <danidurisik@gmail.com>
2022-11-19 09:44:58 +0100

    Implement an embeded redirect.

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT
-const url = process.env.URL
+const redirect = process.env.REDIRECT
 var count = 0
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('*', (req, res) => {
+
   count = ++count
-  res.redirect(url);
+
+  res.render('index', { 
+    title: process.env.TITLE,
+    description: process.env.DESCRIPTION,
+    url: process.env.URL,
+    img: process.env.IMG,
+    color: process.env.COLOR,
+    redirect: process.env.REDIRECT
+  });
+
   console.log(`${count}# Redirected a request`)
+
 });
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
-  console.log(url);
+  console.log(redirect);
 });
